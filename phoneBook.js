354faa6816039f999c2728d1c3db5d7efd3532b6
natamaki <natamaki@yandex.ru>
2016-02-14 13:46:28 +0300

    Update phoneBook.js

--- phoneBook.js
+++ phoneBook.js
@@ -2,17 +2,19 @@
 
 var phoneBook = {};
 
-/*
-   Функция добавления записи в телефонную книгу.
-   На вход может прийти что угодно, будьте осторожны.
-*/
+/**
+ * Добавляет записи в телефонную книгу
+ * @param {String} name
+ * @param {String} phone
+ * @param {String} email
+ */
 module.exports.add = function add(name, phone, email) {
     if (phoneBook[name]) {
         console.log('Это имя уже существует');
         return;
     }
 
-    if(!isPhoneValid(phone) || !isEmailValid(email)) {
+    if(!isNameValid(name) || !isPhoneValid(phone) || !isEmailValid(email)) {
         console.log('Запись не добавлена. Данные не валидны');
         return;
     }
@@ -23,62 +25,62 @@ module.exports.add = function add(name, phone, email) {
     };
 };
 
-/*
-   Функция поиска записи в телефонную книгу.
-   Поиск ведется по всем полям.
-*/
-module.exports.find = function find(query) {
+/**
+ * Ищет записи в телефонной книге по всем полям.
+ * @param {String} query Искомая строка
+ * @param {Boolean} isCalledAnyMethod Вызван из другого метода
+ * @returns {String[]} Массив найденных имен
+ */
+module.exports.find = function find(query, isCalledAnyMethod) {
+    var findedRecordsNames = [];
+
     Object.keys(phoneBook).forEach(function (name) {
         var value = phoneBook[name];
-        if (!query) {
-            printFindedRecords (name, value.phone, value.email)
-        } else if (name.indexOf(query) !== -1 || value.phone.indexOf(query) !== -1 || value.email.indexOf(query) !== -1) {
-            printFindedRecords (name, value.phone, value.email)
+        var queryResult = name.indexOf(query) !== -1 || value.phone.indexOf(query) !== -1 || value.email.indexOf(query) !== -1;
+
+        if (!query || queryResult) {
+            !isCalledAnyMethod && printFindedRecords(name, value.phone, value.email);
+
+            findedRecordsNames.push(name);
         }
     });
+
+    return findedRecordsNames;
 };
 
-/*
-   Функция удаления записи в телефонной книге.
-*/
+/**
+ * Удаляет записи из телефонной книги
+ * @param {String} query Искомая строка
+ */
 module.exports.remove = function remove(query) {
-    var keys = Object.keys(phoneBook).filter(function (key) {
-        return key.indexOf(query) !== -1;
+    var items = module.exports.find(query, true);
+
+    items.forEach(function (item) {
+        delete phoneBook[item];
     });
-
-    if (keys.length) {
-        keys.forEach(function (key) {
-            delete phoneBook[key];
-        });
-        console.log(getDeletedMessage(keys.length));
-    }
+    console.log(getDeletedMessage(items.length));
 };
 
-/*
-   Функция импорта записей из файла (задача со звёздочкой!).
-*/
-module.exports.importFromCsv = function importFromCsv(filename) {
-    var data = require('fs').readFileSync(filename, 'utf-8');
-
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
-};
-
-/*
-   Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
-*/
-module.exports.showTable = function showTable() {
-
-    // Ваша чёрная магия здесь
-
-};
-
+/**
+ * Выводит найденную запись
+ * @param {String} name
+ * @param {String} phone
+ * @param {String} email
+ */
 function printFindedRecords (name, phone, email) {
     console.log(name + ', ' + phone + ', ' + email);
 }
 
+/**
+ * Возвращает текст о кол-ве удаленных контактов
+ * @param {Number} num Кол-во удаленных контактов
+ * @returns {String}
+ */
 function getDeletedMessage(num) {
+    if (num === 0) {
+        return 'Удалять нечего';
+    }
+
     var pluralContacts = {
         1: 'контакт',
         2: 'контакта',
@@ -92,23 +94,47 @@ function getDeletedMessage(num) {
     var deleted;
     var contacts;
     var units = num % 10;
-    if (units >= 2 && units < 5) {
-        contacts = pluralContacts[2];
-        deleted = deletedContacts[2];
-    } else if (units >= 5) {
+    if ((num > 10 && num < 20) || units === 0) {
         contacts = pluralContacts[5];
         deleted = deletedContacts[2];
     } else {
-        contacts = pluralContacts[1];
-        deleted = deletedContacts[1];
+        if (units >= 2 && units < 5) {
+            contacts = pluralContacts[2];
+            deleted = deletedContacts[2];
+        } else if (units >= 5) {
+            contacts = pluralContacts[5];
+            deleted = deletedContacts[2];
+        } else {
+            contacts = pluralContacts[1];
+            deleted = deletedContacts[1];
+        }
     }
     return deleted + ' ' + num + ' ' + contacts;
 }
 
-function isPhoneValid (phone) {
-   return /^\+?\d*\s*(\d{3}|\(\d{3}\))\s*\d{3}(\s|-)?\d(\s|-)?\d{3}$/gim.test(phone);
+/**
+ * Валидирует имя
+ * @param {String} name
+ * @returns {Boolean}
+ */
+function isNameValid (name) {
+   return name && typeof name === 'string';
 }
 
+/**
+ * Валидирует номер телефона
+ * @param {String} phone
+ * @returns {Boolean}
+ */
+function isPhoneValid (phone) {
+   return /^\+?\d*\s*(\d{3}|\(\d{3}\))\s*(\d{7}|(\d{3}(\s|-)?\d(\s|-)?\d{3})|\d{3}(\s|-)?\d{2}(\s|-)?\d{2})$/gim.test(phone);
+}
+
+/**
+ * Валидирует email
+ * @param {String} email
+ * @returns {Boolean}
+ */
 function isEmailValid (email) {
-    return /^\w+@[\wа-яё-]+(\.[\wа-яё]{2,3})+/gim.test(email);
+    return /^\w+@[\wа-яё-]+(\.[\wа-яё]{2,3})+$/gim.test(email);
 }
